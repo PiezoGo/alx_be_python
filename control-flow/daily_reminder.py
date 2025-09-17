519e83d5253b9a532dee532ddc27c702c1379912
PiezoGo <piezo5go@gmail.com>
2025-09-17 05:04:23 +0300

    new

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,20 +1,20 @@
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
-timebound = input("Is it time-bound? (yes/no): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
 match priority:
     case "high":
-        if timebound == "yes":
+        if time_bound == "yes":
             print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
         else:
             print(f"Reminder: '{task}' is a high priority task that needs to be attended to!")
     case "medium":
-        if timebound == "yes":
+        if time_bound == "yes":
             print(f"Reminder: '{task}' is a medium priority task that requires immediate attention today!")
         else:
             print(f"Reminder: '{task}' is a medium priority task that needs to be attended to!")
     case "low":
-        if timebound == "yes":
+        if time_bound == "yes":
             print(f"Note: '{task}' is a low priority task but still need to be done today!")
         else:
             print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time")
