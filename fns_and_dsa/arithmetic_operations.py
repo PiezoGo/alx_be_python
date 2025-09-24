ee22a8343749b9c4a5b34a794a0a30805e6b3dac
PiezoGo <piezo5go@gmail.com>
2025-09-24 09:25:54 +0300

    I think all the files should be complete by now

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,16 @@
 def perform_operation(num1,num2,operation):
     if operation == 'add':
-        sum = num1+num2
+        print(num1+num2)
     elif operation == 'subtract':
-        sum = num1-num2
+        print(num1-num2)
     elif operation == 'multiply':
-        sum = num1*num2
+        print(num1*num2)
     elif operation == 'divide':
         if num2 != 0:
-            sum = num1/num2
+            print(num1/num2)
         else:
             print("You are attempting to divide by 0!!")
     else:
         print("Please enter a valid operation!")
-    return sum
 
 
