d83133e7faddfbcd2a88beb66422039d8625d3d0
PiezoGo <piezo5go@gmail.com>
2025-09-24 09:13:20 +0300

    I think all the files sjould be complete by nowgit add .

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,16 @@
+from datetime import datetime,timedelta
+
+
+def display_current_datetime():
+    current_date = datetime.now()
+    print("Current date and time: ", current_date.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+def calculate_future_date():
+    current_date = datetime.now()
+    number_of_days= int(input("Enter the number of days to add to the current date: "))
+    future_date =current_date + timedelta(days=number_of_days)
+    print("Future date: ", future_date.strftime("%Y-%m-%d %H:%M:%S"))
+
+display_current_datetime()
+calculate_future_date()
---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,23 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
+
+CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
+def convert_to_celsius(fahrenheit):
+    celsius = (fahrenheit-32)*FAHRENHEIT_TO_CELSIUS_FACTOR
+    print(f"{fahrenheit}°F is {celsius}°C")
+
+def convert_to_fahrenheit(celsius):
+    fahrenheit = (celsius*CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    print(f"{celsius}°C is {fahrenheit}°F")
+
+temp = int(input("Enter the temperature to convert: "))
+factor = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+
+if factor == 'C':
+    convert_to_fahrenheit(temp)
+elif factor == 'F':
+    convert_to_celsius(temp)
+
+
+
+
+
