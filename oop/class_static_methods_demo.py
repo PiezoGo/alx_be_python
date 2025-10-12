55d76cd3483840eb3b7386a23b7155d2bb2206a0
PiezoGo <piezo5go@gmail.com>
2025-10-12 16:33:38 +0300

    task 2 is totally complete

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,21 @@
+# class Calculator:
+#     @classmethod
+#     def multiply(cls, a, b):
+
+
+#     @staticmethod
+#     def add(a, b):
+#         return a+b
+    
+
+class Calculator:
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
